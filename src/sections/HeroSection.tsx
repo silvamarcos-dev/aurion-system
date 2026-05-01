bc6b76d756d0966e7af195192910eef7f9a27f10
Marcos Silva <1609.marcos.silva@gmail.com>
2026-05-01 12:45:52 -0300

    erros de gramatica corrigidos

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -51,7 +51,7 @@ export default function HeroSection() {
           >
             Pare de usar uma IA que apenas responde.
             <br />
-            Use uma IA que conversa, entende e se adapta a você.
+            Use uma IA que pensa, organiza e aprende com você.
             <br />
             <RotatingText />
           </motion.p>
